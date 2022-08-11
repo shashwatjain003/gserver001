df8173c7301a6a6dc14b5145a6c4d5f123529dae
shashwatjain003 <66857470+shashwatjain003@users.noreply.github.com>
2022-08-11 21:09:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,22 +10,24 @@ res.send('Server is running')
 });
 io.on('connection', (socket) => {
 console.log('user connected')
-//socket.on('join', function(userNickname) {
-//        console.log(userNickname +" : has joined the chat "  );
-//        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined");
-//    });
+        
+socket.on('played', function(dane) {
+        socket.broadcast.emit('play_res',dane);
+    });
 socket.on('messagedetection', (id,x,y,devid) => {
         //create a message object 
        let  message = {"x":x, "id":id,"y":y,"devid":devid}
           // send the message to the client side  
        io.emit('message', message );
-     
       });
-socket.on('played', (dane,devids) => {
-       let  play_res = {"dane":dane,devids":devids}
+        
+        
+//socket.on('played', (dane,devids) => {
+//       let  play_res = {"dane":dane,devids":devids}
           // send the message to the client side  
-       io.emit('play_res', play_res );
-      });
+//       io.emit('play_res', play_res );
+//      });
+        
 });
 server.listen(port,()=>{
 console.log('Node app running');
